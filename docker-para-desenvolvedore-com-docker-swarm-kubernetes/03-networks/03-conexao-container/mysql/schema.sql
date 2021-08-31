8e8c0fc1fe046dd63ea44bc68e140d0f2113291b
Rafael Augusto Miranda <rafael.miranda@grupomult.com.br>
2021-08-31 20:07:03 -0300

    commit

---
+++ docker-para-desenvolvedore-com-docker-swarm-kubernetes/03-networks/03-conexao-container/mysql/schema.sql
@@ -0,0 +1,8 @@
+CREATE flaskdocker;
+USE flaskdocker;
+
+CREATE TABLE `flaskdocker`.`user` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    PRIMARY KEY (ID);
+)
---
+++ docker-para-desenvolvedore-com-docker-swarm-kubernetes/05-compose/06-bind-mount-compose/mysql/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE flaskdocker;
+USE flaskdocker;
+
+CREATE TABLE `flaskdocker`.`user` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    PRIMARY KEY (ID);
+)
